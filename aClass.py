8a51ab2eb42522127415556d28d6d368c790f0dd
microslaw <milosz.grunwald@gmail.com>
2024-04-14 21:27:53 +0200

    add matching course years to courses generation

--- aClass.py
+++ aClass.py
@@ -31,9 +31,10 @@ def generate(
 
     df = pd.DataFrame()
     df.insert(0, "tmpID", range(count * MaxSchoolYears))
-    df["Year"] = (df["tmpID"] % MaxSchoolYears)+1
-    df["Specialization"] = np.random.choice(dictToKeyList(specializations), count * MaxSchoolYears)
-    df["PrefferedCourses"] = df["Specialization"].apply(lambda x: specializations[x])
+    df["Year"] = (df["tmpID"] % MaxSchoolYears) + 1
+    df["Specialization"] = np.random.choice(
+        dictToKeyList(specializations), count * MaxSchoolYears
+    )
     df["ClassName"] = numbersToLetters(df["tmpID"] // MaxSchoolYears)
     df["ClassName"] = df["ClassName"] + (presentYear - df["Year"]).astype(str).apply(
         lambda x: x[2:]
@@ -41,37 +42,68 @@ def generate(
     df["tID"] = np.random.choice(teachers["tID"], count * MaxSchoolYears)
     # df.insert(0, "courses", [])
 
-
-
     df_homeroom_courses = pd.DataFrame()
-    df_homeroom_courses.insert(0, "cID", range(len(courses), len(courses) + len(df)))
-    df_homeroom_courses["Name"] = "Homeroom"
-    df_homeroom_courses["Year"] = df["Year"]
-    df_homeroom_courses["tID"] = df["tID"]
-    df_homeroom_courses["ClassName"] = df["ClassName"]
+
+    id_start = len(courses)
+    for year in range(0, MaxSchoolYears):
+
+        df_yearly_homeroom_courses = pd.DataFrame()
+        df_yearly_homeroom_courses.insert(0, "cID", range(id_start, id_start + len(df)))
+        id_start += len(df)
+        df_yearly_homeroom_courses["Name"] = "Homeroom"
+        df_yearly_homeroom_courses["Year"] = df["Year"] + year
+        df_yearly_homeroom_courses["tID"] = df["tID"]
+        df_yearly_homeroom_courses["ClassName"] = df["ClassName"]
+        df_homeroom_courses = pd.concat(
+            [df_homeroom_courses, df_yearly_homeroom_courses]
+        )
 
     def getRandomCourses(specialization, courses, courseCount, year):
-        prefferedCourses = courses[courses["Name"].isin(specializations[specialization])]
-        chosenPrefferedCourses = [x for x in np.random.choice(prefferedCourses["cID"], size=(courseCount//2))]
-        chosenNormalCourses = [x for x in np.random.choice(courses["cID"], size=min(courseCount - (courseCount//2),len(courses)), replace=False)]
+        chosenPrefferedCourses = []
+        chosenNormalCourses = []
+
+        for course_year in range(year, MaxSchoolYears + 1):
+            age_matching_courses = courses[courses["Year"] == course_year]
+            prefferedCourses = age_matching_courses[
+                age_matching_courses["Name"].isin(specializations[specialization])
+            ]
+            prefferedCourses = [
+                x
+                for x in np.random.choice(prefferedCourses["cID"], size=(courseCount // 2))
+            ]
+
+            normalCourses = [
+                x
+                for x in np.random.choice(
+                    age_matching_courses["cID"],
+                    size=min(courseCount - (courseCount // 2), len(age_matching_courses)),
+                    replace=False,
+                )
+            ]
+            chosenPrefferedCourses.extend(prefferedCourses)
+            chosenNormalCourses.extend(normalCourses)
         return chosenPrefferedCourses + chosenNormalCourses
 
     for year in courses["Year"].unique():
-        appropriateCourses = courses[courses["Year"] == year]
         df.loc[df["Year"] == year, "courses"] = df["Specialization"].apply(
-            getRandomCourses, args=(appropriateCourses, courseCount, year)
+            getRandomCourses, args=(courses, courseCount, year)
         )
 
-    df["homeroomCourse"] = [
+    df["homeroomCourses"] = [
         [
             df_homeroom_courses[df_homeroom_courses["ClassName"] == row["ClassName"]][
                 "cID"
-            ].values[0]
+            ]
         ]
         for index, row in df.iterrows()
     ]
 
-    df["courses"] = df["courses"] + df["homeroomCourse"]
+    df["homeroomCourses"] = [x[0].values for x in df["homeroomCourses"]]
+
+    for index, row in df.iterrows():
+        row["courses"].extend(row["homeroomCourses"])
+
+    # df["courses"] = df["homeroomCourses"]
 
     df_homeroom_courses = df_homeroom_courses[["cID", "Name", "Year", "tID"]]
 
