ef0bddc23d57e7401c9a10fd046a6431706db916
microslaw <milosz.grunwald@gmail.com>
2024-05-16 08:22:38 +0200

    automate 2nd snapshot

--- parameters.py
+++ parameters.py
@@ -1,8 +1,8 @@
 classroom_count = 10  # How many classrooms are there in the school
-teacher_count = 10  # How many teachers work in the school
-course_count = 20  # How many courses are there in the school
+teacher_count = 20  # How many teachers work in the school
+course_count = 100  # How many courses are there in the school
 class_count = 3  # How many classes per year are there in the school; If max_school_years = 6, will generate 60 classes
-student_count = 20  # How many students are there in the school
+student_count = 100  # How many students are there in the school
 
 # Count's of tuples below are determined by different parameters. Setting count other than 0, will cutoff remaining tuples
 # This may break the integrity of the data
@@ -41,8 +41,9 @@ attendance_prob = 0.99 # probability of a student attending a meeting without be
 excused_prob = 0.7 # probability of a student recorded as not attending a meeting, has been excused
 late_prob = 0.2 # probability of a student recorded as not attending a meeting, but being late
 
-start_date = "2020-09-10" # first day of generated data
-end_date =  "2021-06-20" # last day of generated data
+start_date = "2020-09-01" # first day of generated data
+end_date =  "2021-08-01" # last day of generated data
+end_date2 = "2022-08-01" # last day of generated data for the second snapshot
 
 
 default_city_districts = [
